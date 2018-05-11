a67c8a435f680da6f8c1ab7f4293f6cde6ebddb6
bornagainunitologist <cintari939@gmail.com>
2018-05-10 21:15:06 -0400

    Add files via upload

---
+++ useful code examples/basichttprequestexample/basichttprequestexample.ino
@@ -0,0 +1,64 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "your_ssid";
+const char* password = "your_pass";
+
+
+const char* host = "www.example.com";
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println();
+
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" connected");
+
+  WiFiClient client;
+
+  Serial.printf("\n[Connecting to %s ... ", host);
+  if (client.connect(host, 80))
+  {
+    Serial.println("connected]");
+
+    Serial.println("[Sending a request]");
+    client.print(String("GET /") + " HTTP/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n" +
+                 "\r\n"
+                );
+
+    Serial.println("[Response:]");
+    while (client.connected())
+    {
+      if (client.available())
+      {
+        String line = client.readStringUntil('\n');
+        Serial.println(line);
+      }
+    }
+    client.stop();
+    Serial.println("\n[Disconnected]");
+  }
+  else
+  {
+    Serial.println("connection failed!]");
+    client.stop();
+  }
+  
+  
+}
+
+
+void loop()
+{
+  
+  delay(5000);
+}
